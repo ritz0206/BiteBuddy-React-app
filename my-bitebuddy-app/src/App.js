fde36d083cabaf96b6f4a7c98a5bf2703f7a857b
Deshmukh <ruthika.deshmukh@siemens.com>
2026-01-07 16:29:54 +0530

    Reservation form created

--- my-bitebuddy-app/src/App.js
+++ my-bitebuddy-app/src/App.js
@@ -1,19 +1,62 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './NavigationBar';
 import HeroSection from './HeroSection';
 import OrderOnlineSection from './OrderOnlineSection';
 import AboutUs from './AboutUs';
 import Footer from './Footer';
+import Reserve from './Reserve';
+import React, { useState } from 'react';
 
 function App() {
+    const [formDataTime, setFormDataTime] = useState({
+      date: '',
+      time: ''
+    });
+
+    const handleFormDataTimeChange = (e) => {
+    const { name, value } = e.target;
+    
+    setFormDataTime(prevState => {
+      if (name === 'date' && value === '') {
+        return{
+          ...prevState,
+          [name]: value,
+          time : ''
+        };
+      }
+      else if (name === 'date' && value === "2026-01-17") {
+        return{
+          ...prevState,
+          [name]: value,
+          time : ['18:00', '19:00', '20:00']
+        };
+      }
+      return {
+      ...prevState,
+      [name]: value,
+      time: ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
+    };
+    });
+  };
   return (
-    <>
+    <Router>
       <NavigationBar />
-      <HeroSection />
-      <OrderOnlineSection />
-      <AboutUs/>
-      <Footer/>
-    </>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <HeroSection />
+              <OrderOnlineSection />
+              <AboutUs />
+            </>
+          }
+        />
+        <Route path="/reserve" element={<Reserve formDataTime={formDataTime} handleFormDataTimeChange={handleFormDataTimeChange} />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
